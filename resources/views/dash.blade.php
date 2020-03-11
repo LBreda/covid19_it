755f77fb31176ae920f66a8c7d6dd74ad5a23279
lorenzo <lorenzo@lbreda.com>
2020-03-11 19:51:34 +0100

    :sparkles: Matomo Support

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -103,6 +103,29 @@
                 <a href="https://github.com/LBreda/covid19_it/issues">segnalare bug qui</a>.</p>
         </div>
     </footer>
+
+    <!-- Matomo -->
+    <script type="text/javascript">
+        var _paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(["disableCookies"]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function () {
+            var u = "{{ env('MATOMO_URL') }}";
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '{{ env('MATOMO_SITE_ID') }}']);
+            var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+            g.type = 'text/javascript';
+            g.async = true;
+            g.defer = true;
+            g.src = u + 'matomo.js';
+            s.parentNode.insertBefore(g, s);
+        })();
+    </script>
+    <noscript><p><img src={{ env('MATOMO_URL') }}/matomo.php?idsite={{ env('MATOMO_SITE_ID') }}&amp;rec=1" style="border:0;" alt=""/></p>
+    </noscript>
+    <!-- End Matomo Code -->
 @stop
 
 @section('js')
