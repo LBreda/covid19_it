66db907cfa6ba23f18cb0fb2ce4c182a913f812a
lorenzo <lorenzo@lbreda.com>
2020-03-13 19:48:57 +0100

    :bulb: Autodoc

--- app/Models/Notice.php
+++ app/Models/Notice.php
@@ -4,6 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Notice
+ *
+ * @property int $id
+ * @property int|null $region_id
+ * @property \Illuminate\Support\Carbon $date
+ * @property string $notice
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Region|null $region
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice whereNotice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice whereRegionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Notice whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Notice extends Model
 {
     protected $table = 'notices';
--- app/Models/Region.php
+++ app/Models/Region.php
@@ -4,6 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Region
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $code
+ * @property float $latitude
+ * @property float $longitude
+ * @property string|null $phone
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Notice[] $notices
+ * @property-read int|null $notices_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region whereLatitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region whereLongitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Region extends Model
 {
     protected $table = "regions";
