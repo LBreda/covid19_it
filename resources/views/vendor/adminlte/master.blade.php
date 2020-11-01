6329cdc17e304710017ba3ade4dd875b5fd79746
lorenzo <lorenzo@lbreda.com>
2020-11-01 15:03:07 +0100

    :sparkles: Better favicons set

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -32,22 +32,23 @@
         <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}"/>
     @elseif(config('adminlte.use_full_favicon'))
         <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}"/>
-        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/apple-icon-57x57.png') }}">
-        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicons/apple-icon-60x60.png') }}">
-        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicons/apple-icon-72x72.png') }}">
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicons/apple-icon-76x76.png') }}">
-        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicons/apple-icon-114x114.png') }}">
-        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicons/apple-icon-120x120.png') }}">
-        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicons/apple-icon-144x144.png') }}">
-        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicons/apple-icon-152x152.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-icon-180x180.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/favicon-96x96.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicons/android-icon-192x192.png') }}">
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicons/60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicons/72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicons/76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicons/114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicons/120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicons/144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicons/152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/180.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/16.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/96.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicons/192.png') }}">
+        <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicons/512.png') }}">
         <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="{{ asset('favicon/ms-icon-144x144.png') }}">
+        <meta name="msapplication-TileColor" content="#343a40">
+        <meta name="msapplication-TileImage" content="{{ asset('favicon/144.png') }}">
     @endif
 </head>
 <body class="@yield('classes_body')" @yield('body_data')>
