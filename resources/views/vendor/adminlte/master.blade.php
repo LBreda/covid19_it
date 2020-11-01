8035432dbee781b3be9b9e06a08bfcde8eaf5dba
lorenzo <lorenzo@lbreda.com>
2020-11-01 23:23:45 +0100

    :fire: Removed unnecessary js dependencies

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -8,23 +8,8 @@
     <title>@yield('title_prefix', config('adminlte.title_prefix', ''))
         @yield('title', config('adminlte.title', 'AdminLTE 3'))
         @yield('title_postfix', config('adminlte.title_postfix', ''))</title>
-    @if(! config('adminlte.enabled_laravel_mix'))
 
-        <script src="https://kit.fontawesome.com/3fbc50dc3b.js" crossorigin="anonymous"></script>
-
-        @include('adminlte::plugins', ['type' => 'css'])
-
-        @yield('adminlte_css_pre')
-
-        <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
-
-        @yield('adminlte_css')
-
-        <link rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-    @else
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    @endif
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     @yield('meta_tags')
 
