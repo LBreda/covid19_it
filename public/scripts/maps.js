1dee4335714bff136aee6b113dcb677101454c95
lorenzo <lorenzo@lbreda.com>
2021-01-24 19:18:04 +0100

    :recycle: Refactoring and cleanup

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,129 +1,61 @@
-function getColorIll(min, max, value) {
-    let interval = Math.round((max - min) * 100 / 8) / 100;
-    return value > interval * 8 ? '#800026' :
-        value > interval * 7 ? '#BD0026' :
-            value > interval * 6 ? '#E31A1C' :
-                value > interval * 5 ? '#FC4E2A' :
-                    value > interval * 4 ? '#FD8D3C' :
-                        value > interval * 3 ? '#FEB24C' :
-                            value > interval * 2 ? '#FED976' :
-                                '#FFEDA0';
+function mapdataToArray(mapData, key) {
+    return [...Object.keys(mapData).map(k => mapData[k][key])];
 }
 
-function getColorInfected(min, max, value) {
-    let interval = Math.round((max - min) * 100 / 8) / 100;
-    return value > interval * 8 ? '#4a1486' :
-        value > interval * 7 ? '#6a51a3' :
-            value > interval * 6 ? '#807dba' :
-                value > interval * 5 ? '#9e9ac8' :
-                    value > interval * 4 ? '#bcbddc' :
-                        value > interval * 3 ? '#dadaeb' :
-                            value > interval * 2 ? '#efedf5' :
-                                '#fcfbfd';
+function getColor(dataset, value, colors) {
+    let min = Math.min(...dataset);
+    let max = Math.max(...dataset);
+    let step = Math.round((max - min) * 100 / (colors.length - 1));
+    let interval_no = Math.floor((value - min) * 100 / step);
+    console.log(interval_no);
+    return colors[interval_no];
 }
 
-function getColorDead(min, max, value) {
-    let interval = Math.round((max - min) * 100 / 8) / 100;
-    return value > interval * 8 ? '#252525' :
-        value > interval * 7 ? '#525252' :
-            value > interval * 6 ? '#737373' :
-                value > interval * 5 ? '#969696' :
-                    value > interval * 4 ? '#bdbdbd' :
-                        value > interval * 3 ? '#d9d9d9' :
-                            value > interval * 2 ? '#f0f0f0' :
-                                '#ffffff';
-}
-
-function getColorTested(min, max, value) {
-    let interval = Math.round((max - min) * 100 / 8) / 100;
-    return value > interval * 8 ? '#005a32' :
-        value > interval * 7 ? '#238b45' :
-            value > interval * 6 ? '#41ab5d' :
-                value > interval * 5 ? '#74c476' :
-                    value > interval * 4 ? '#a1d99b' :
-                        value > interval * 3 ? '#c7e9c0' :
-                            value > interval * 2 ? '#edf8e9' :
-                                '#ffffff';
-}
-
-function getColorDoses(min, max, value) {
-    let interval = Math.round((max - min) * 100 / 8) / 100;
-    return value > interval * 8 ? '#C71585' :
-        value > interval * 7 ? '#CF3696' :
-            value > interval * 6 ? '#D758A8' :
-                value > interval * 5 ? '#DF79B9' :
-                    value > interval * 4 ? '#E79BCB' :
-                        value > interval * 3 ? '#EFBCDC' :
-                            value > interval * 2 ? '#F7DEEE' :
-                                '#ffffff';
-}
+let colors_ill = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026',];
+let colors_infected = ['#fcfbfd', '#efedf5', '#dadaeb', '#bcbddc', '#9e9ac8', '#807dba', '#6a51a3', '#4a1486',];
+let colors_dead = ['#ffffff', '#f0f0f0', '#d9d9d9', '#bdbdbd', '#969696', '#737373', '#525252', '#252525',];
+let colors_tested = ['#ffffff', '#edf8e9', '#c7e9c0', '#a1d99b', '#74c476', '#41ab5d', '#238b45', '#005a32',];
+let colors_doses = ['#ffffff', '#F7DEEE', '#EFBCDC', '#E79BCB', '#DF79B9', '#D758A8', '#CF3696', '#C71585',]
 
 // Maps
-let map_ill = L.map('map_ill', {
+let map_options = {
     zoomSnap: 0.2,
     dragging: !L.Browser.mobile,
     tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
-let map_infected = L.map('map_infected', {
-    zoomSnap: 0.2,
-    dragging: !L.Browser.mobile,
-    tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
-let map_dead = L.map('map_dead', {
-    zoomSnap: 0.2,
-    dragging: !L.Browser.mobile,
-    tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
-let map_tested = L.map('map_tested', {
-    zoomSnap: 0.2,
-    dragging: !L.Browser.mobile,
-    tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
-let map_restrictions = L.map('map_restrictions', {
-    zoomSnap: 0.2,
-    dragging: !L.Browser.mobile,
-    tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
-let map_doses = L.map('map_doses', {
-    zoomSnap: 0.2,
-    dragging: !L.Browser.mobile,
-    tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
-let map_final_doses = L.map('map_final_doses', {
-    zoomSnap: 0.2,
-    dragging: !L.Browser.mobile,
-    tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
-let map_vaccine_shipments = L.map('map_vaccine_shipments', {
-    zoomSnap: 0.2,
-    dragging: !L.Browser.mobile,
-    tap: !L.Browser.mobile
-}).setView([41.893056, 12.482778], 5).fitBounds([[47.0727778, 6.6255556], [35.49, 18.5216667]]);
+};
+let map_view = [41.893056, 12.482778];
+let map_zoom = 5;
+let map_bounds = [[47.0727778, 6.6255556], [35.49, 18.5216667]];
+let map_tiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let map_tiles_options = {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+};
+let map_style = {
+    fillColor: '',
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7
+}
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_ill);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_infected);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_dead);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_tested);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_restrictions);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_doses);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_final_doses);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map_vaccine_shipments);
+let map_ill = L.map('map_ill', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+let map_infected = L.map('map_infected', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+let map_dead = L.map('map_dead', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+let map_tested = L.map('map_tested', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+let map_restrictions = L.map('map_restrictions', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+let map_doses = L.map('map_doses', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+let map_final_doses = L.map('map_final_doses', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+let map_vaccine_shipments = L.map('map_vaccine_shipments', map_options).setView(map_view, map_zoom).fitBounds(map_bounds);
+
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_ill);
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_infected);
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_dead);
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_tested);
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_restrictions);
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_doses);
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_final_doses);
+L.tileLayer(map_tiles, map_tiles_options).addTo(map_vaccine_shipments);
 
 let regionsReq = new XMLHttpRequest();
 regionsReq.open('GET', document.getElementById('js-maps').dataset.geoUrl);
@@ -135,31 +67,12 @@ regionsReq.onload = () => {
     mapDataReq.responseType = 'json';
     mapDataReq.onload = () => {
         let mapData = mapDataReq.response;
-        let minIll = Math.min(...Object.keys(mapData).map(key => mapData[key].ill));
-        let maxIll = Math.max(...Object.keys(mapData).map(key => mapData[key].ill));
-        let minInfected = Math.min(...Object.keys(mapData).map(key => mapData[key].infected));
-        let maxInfected = Math.max(...Object.keys(mapData).map(key => mapData[key].infected));
-        let minDead = Math.min(...Object.keys(mapData).map(key => mapData[key].dead));
-        let maxDead = Math.max(...Object.keys(mapData).map(key => mapData[key].dead));
-        let minTested = Math.min(...Object.keys(mapData).map(key => mapData[key].tested));
-        let maxTested = Math.max(...Object.keys(mapData).map(key => mapData[key].tested));
-        let minDoses = Math.min(...Object.keys(mapData).map(key => mapData[key].daily_doses));
-        let maxDoses = Math.max(...Object.keys(mapData).map(key => mapData[key].daily_doses));
-        let minFinalDoses = Math.min(...Object.keys(mapData).map(key => mapData[key].daily_final_doses));
-        let maxFinalDoses = Math.max(...Object.keys(mapData).map(key => mapData[key].daily_final_doses));
-        let minVaccineShipments = Math.min(...Object.keys(mapData).map(key => mapData[key].daily_vaccine_shipments));
-        let maxVaccineShipments = Math.max(...Object.keys(mapData).map(key => mapData[key].daily_vaccine_shipments));
 
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: getColorIll(minIll, maxIll, mapData[feature.properties.DatabaseID].ill),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                }
+                let style = map_style;
+                style.fillColor = getColor(mapdataToArray(mapData, 'ill'), mapData[feature.properties.DatabaseID].ill, colors_ill);
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}: ${mapData[feature.properties.DatabaseID].ill}`)
@@ -167,14 +80,9 @@ regionsReq.onload = () => {
         }).addTo(map_ill);
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: getColorInfected(minInfected, maxInfected, mapData[feature.properties.DatabaseID].infected),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                }
+                let style = map_style;
+                style.fillColor = getColor(mapdataToArray(mapData, 'infected'), mapData[feature.properties.DatabaseID].infected, colors_infected)
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}: ${mapData[feature.properties.DatabaseID].infected}`)
@@ -182,14 +90,9 @@ regionsReq.onload = () => {
         }).addTo(map_infected);
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: getColorDead(minDead, maxDead, mapData[feature.properties.DatabaseID].dead),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                }
+                let style = map_style;
+                style.fillColor = getColor(mapdataToArray(mapData, 'dead'), mapData[feature.properties.DatabaseID].dead, colors_dead)
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}: ${mapData[feature.properties.DatabaseID].dead}`)
@@ -197,14 +100,9 @@ regionsReq.onload = () => {
         }).addTo(map_dead);
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: getColorTested(minTested, maxTested, mapData[feature.properties.DatabaseID].tested),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                }
+                let style = map_style;
+                style.fillColor = getColor(mapdataToArray(mapData, 'tested'), mapData[feature.properties.DatabaseID].tested, colors_tested)
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}: ${mapData[feature.properties.DatabaseID].tested}`)
@@ -212,14 +110,9 @@ regionsReq.onload = () => {
         }).addTo(map_tested);
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: mapData[feature.properties.DatabaseID].severity.color,
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.9
-                }
+                let style = map_style;
+                style.fillColor = mapData[feature.properties.DatabaseID].severity.color
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}`)
@@ -227,14 +120,9 @@ regionsReq.onload = () => {
         }).addTo(map_restrictions);
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: getColorDoses(minDoses, maxDoses, mapData[feature.properties.DatabaseID].daily_doses),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.9
-                }
+                let style = map_style;
+                style.fillColor = getColor(mapdataToArray(mapData, 'daily_doses'), mapData[feature.properties.DatabaseID].daily_doses, colors_doses)
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}: ${mapData[feature.properties.DatabaseID].daily_doses}`)
@@ -242,14 +130,9 @@ regionsReq.onload = () => {
         }).addTo(map_doses);
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: getColorDoses(minFinalDoses, maxFinalDoses, mapData[feature.properties.DatabaseID].daily_final_doses),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.9
-                }
+                let style = map_style;
+                style.fillColor = getColor(mapdataToArray(mapData, 'daily_final_doses'), mapData[feature.properties.DatabaseID].daily_final_doses, colors_doses)
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}: ${mapData[feature.properties.DatabaseID].daily_final_doses}`)
@@ -257,14 +140,9 @@ regionsReq.onload = () => {
         }).addTo(map_final_doses);
         L.geoJson(regionsReq.response, {
             style: (feature) => {
-                return {
-                    fillColor: getColorDoses(minVaccineShipments, maxVaccineShipments, mapData[feature.properties.DatabaseID].daily_vaccine_shipments),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.9
-                }
+                let style = map_style;
+                style.fillColor = getColor(mapdataToArray(mapData, 'daily_vaccine_shipments'), mapData[feature.properties.DatabaseID].daily_vaccine_shipments, colors_doses)
+                return style;
             },
             onEachFeature: (feature, layer) => {
                 layer.bindTooltip(`${feature.properties.Regione}: ${mapData[feature.properties.DatabaseID].daily_vaccine_shipments}`)
