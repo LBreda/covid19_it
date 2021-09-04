1d7e62700cb9d2e6998ff6161cc0131374860f19
Lorenzo Breda <lorenzo@lbreda.com>
2021-09-04 16:39:50 +0200

    :bento: Apple splash screens

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -32,7 +32,8 @@
 
         <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     @else
         <link rel="stylesheet" href="{{ mix(config('adminlte.laravel_mix_css_path', 'css/app.css')) }}">
     @endif
@@ -42,7 +43,7 @@
         @if(app()->version() >= 7)
             @livewireStyles
         @else
-            <livewire:styles />
+            <livewire:styles/>
         @endif
     @endif
 
@@ -51,7 +52,7 @@
 
     {{-- Favicon --}}
     @if(config('adminlte.use_ico_only'))
-        <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" />
+        <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}"/>
     @elseif(config('adminlte.use_full_favicon'))
         <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}"/>
         <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/57.png') }}">
@@ -68,6 +69,36 @@
         <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/96.png') }}">
         <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicons/192.png') }}">
         <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicons/512.png') }}">
+        <link href="{{ asset('splashscreens/iphone5_splash.png") }}"
+              media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/iphone6_splash.png") }}"
+              media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/iphoneplus_splash.png") }}"
+              media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/iphonex_splash.png") }}"
+              media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/iphonexr_splash.png") }}"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/iphonexsmax_splash.png") }}"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/ipad_splash.png") }}"
+              media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/ipadpro1_splash.png") }}"
+              media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/ipadpro3_splash.png") }}"
+              media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"/>
+        <link href="{{ asset('splashscreens/ipadpro2_splash.png") }}"
+              media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"/>
         <meta name="msapplication-TileColor" content="#343a40">
         <meta name="msapplication-TileImage" content="{{ asset('favicon/144.png') }}">
         <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
@@ -89,33 +120,33 @@
 </head>
 
 <body class="@yield('classes_body')" @yield('body_data')>
-    {{-- Body Content --}}
-    @yield('body')
+{{-- Body Content --}}
+@yield('body')
 
-    {{-- Base Scripts --}}
-    @if(!config('adminlte.enabled_laravel_mix'))
-        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+{{-- Base Scripts --}}
+@if(!config('adminlte.enabled_laravel_mix'))
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 
-        {{-- Configured Scripts --}}
-        @include('adminlte::plugins', ['type' => 'js'])
+    {{-- Configured Scripts --}}
+    @include('adminlte::plugins', ['type' => 'js'])
 
-        <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+@else
+    <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
+@endif
+
+{{-- Livewire Script --}}
+@if(config('adminlte.livewire'))
+    @if(app()->version() >= 7)
+        @livewireScripts
     @else
-        <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
+        <livewire:scripts/>
     @endif
+@endif
 
-    {{-- Livewire Script --}}
-    @if(config('adminlte.livewire'))
-        @if(app()->version() >= 7)
-            @livewireScripts
-        @else
-            <livewire:scripts />
-        @endif
-    @endif
-
-    {{-- Custom Scripts --}}
-    @yield('adminlte_js')
+{{-- Custom Scripts --}}
+@yield('adminlte_js')
 </body>
 </html>
