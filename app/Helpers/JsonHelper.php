f3604f2428e48f5a0fd3a010054ff0ed60a3438e
Lorenzo Breda <lorenzo@lbreda.com>
2022-03-26 02:16:07 +0100

    :bug: Less linting

--- app/Helpers/JsonHelper.php
+++ app/Helpers/JsonHelper.php
@@ -13,63 +13,10 @@ class JsonHelper
     public static function lint(string $json): string
     {
         $fixes = [
-            /* Find any character except colons, commas, curly and square brackets surrounded or
-             * not by spaces preceded and followed by spaces, colons, commas, curly or square brackets
-             * and put quotation marks surrounded by spaces between them
-             */
-            "/([\s:,\{}\[\]])\s*'([^:,\{}\[\]]*)'\s*([\s:,\{}\[\]])/" => '$1 "$2" $3',
-
-            /* Find any left curly brackets surrounded or not by one or more of any character except
-             * spaces, colons, commas, curly and square brackets and put spaces between them
-             */
-            '/([^\s:,\{}\[\]]*)\{([^\s:,\{}\[\]]*)/' => '$1 { $2',
-
-            /* Find any right curly brackets preceded by one or more of any character except spaces,
-             * colons, commas, curly and square brackets and put a space between them
-             */
-            "/([^\s:,\{}\[\]]+)}/" => '$1 }',
-
-            /* JSON.parse() doesn't allow trailing commas, so, remove trailing commas of any right curly brackets */
+            // Remove trailing commas in arrays of objects
             "/(}),(\s*+[^{])/" => '$1$2',
 
-            /* Find or not one or more of any character except spaces, colons, commas, curly and square brackets
-             * followed by one or more of any character except spaces, colons, commas, curly and square brackets
-             * and put quotation marks surrounding them
-            */
-            '/([^\s:,\{}\[\]]+\s*)*[^\s:,\{}\[\]]+/' => '"$0"',
-
-            /* Find one or more of quotation marks or/and apostrophes surrounding any character except colons,
-             * commas, curly and square brackets and replace by single quotation marks
-             */
-            '/["\']+([^"\':,\{}\[\]]*)["\']+/' => '"$1"',
-
-            /* Find or not one or more of any character except spaces, colons, commas, curly and square brackets
-             * surrounded by quotation marks followed by one or more spaces and  one or more of any character except
-             * spaces, colons, commas, curly and square brackets and add backslashes to its quotation marks
-             */
-            '/(")([^\s:,\{}\[\]]+)(")(\s+([^\s:,\{}\[\]]+))/' => '\\$1$2\\$3$4',
-
-            /* Find or not one or more of any character except spaces, colons, commas, curly and square brackets
-             * surrounded by apostrophes followed by one or more spaces and  one or more of any character except
-             * spaces, colons, commas, curly and square brackets and add backslashes to its apostrophes;
-             */
-            "/(')([^\s:,\{}\[\]]+)(')(\s+([^\s:,\{}\[\]]+))/" => '\\$1$2\\$3$4',
-
-            /* Find any right curly brackets followed by quotation marks */
-            '/(})(")/' => '$1, $2',
-
-            /* Find any comma followed by one or more spaces and a right curly bracket and replace by a space
-             * followed by a right curly bracket
-             */
-            '/,\s+(})/' => ' $1',
-
-            /* Find one or more spaces and replace by one space */
-            '/\s+/' => ' ',
-
-            /* Find one or more spaces at start of string and remove it */
-            '/^\s+/' => '',
-
-            /* Find any non-printable character and remove it */
+            // Remove non-printable characters
             '/[\x00-\x1F\x80-\xFF]/' => '',
         ];
 
