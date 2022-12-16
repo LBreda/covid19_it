fe4aff78f691d6fdb4f4ff76adf6497f1da5985e
Lorenzo Breda <lorenzo@lbreda.com>
2022-12-16 22:14:45 +0100

    :sparkles: Added date and region_id index

---
+++ database/migrations/2022_12_16_221227_add_date_and_region_index_to_vaccionations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vaccinations', function (Blueprint $table) {
+            $table->index(['date', 'region_id'], 'date_region_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vaccinations', function (Blueprint $table) {
+            $table->dropIndex('date_region_index');
+        });
+    }
+};
